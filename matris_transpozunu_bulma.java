210742d1d93ef8906665ad37e84419bd0ca92afe
Ayşe Nur Sarun <117396227+aysenursarun@users.noreply.github.com>
2024-01-03 02:09:00 +0300

    matris_transpozunu_bulma

---
+++ matris_transpozunu_bulma.java
@@ -0,0 +1,34 @@
+package matris_transpozunu_bulma;
+
+public class matris_transpozunu_bulma {
+
+	public static void main(String[] args) {
+
+		   int[][] matris = {
+	                {1,5,3},
+	                {4,8,6},
+	                {7,3,9},
+	                {1,3,5}
+	        };
+
+	        System.out.println("Matris : ");
+	        for(int[] row: matris){
+	            for(int column: row){
+	                System.out.print(column + " ");
+	            }
+	            System.out.println();
+	        }
+	        
+	        System.out.println();
+
+	        System.out.println("Transpoze : ");
+	        int column = matris[0].length;
+	        for(int i=0; i < column; i++){
+	            for(int j=0; j < matris.length; j++){
+	                System.out.print(matris[j][i] + " ");
+	            }
+	            System.out.println();
+	        }
+	}
+
+}
